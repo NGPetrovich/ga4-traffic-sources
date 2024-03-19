25aa7438cd4b0445cf0bc58c04cc7b57c211a99c
Nikita Gonchareno <nikita_goncharenko@outlook.com>
2024-03-19 16:55:47 +0100

    updated cleaner.py and tested it

--- step-1/cleaner.py
+++ step-1/cleaner.py
@@ -1,10 +1,18 @@
 import csv
+import os
 
-def extract_data_from_date(csv_file, output_file):
+def extract_data_from_date(csv_file, output_file_name):
+    # Define the output folder path relative to the current script location
+    output_folder_path = "../step-2"
+    output_file_path = os.path.join(output_folder_path, output_file_name)
+
+    # Ensure the output folder exists, create if it does not
+    os.makedirs(output_folder_path, exist_ok=True)
+
     found_date = False
     with open(csv_file, 'r', newline='') as file:
         reader = csv.reader(file)
-        with open(output_file, 'w', newline='') as output_csv:
+        with open(output_file_path, 'w', newline='') as output_csv:
             writer = csv.writer(output_csv)
             for row in reader:
                 if found_date and not row[:3] == ['', '', '']:
@@ -13,12 +21,10 @@ def extract_data_from_date(csv_file, output_file):
                     writer.writerow(row)
                     found_date = True
     if found_date:
-        print(f'Extracted data from "Date" onwards to: {output_file}')
+        print(f'Extracted data from "Date" onwards to: {output_file_path}')
     else:
         print('Could not find "Date" in the CSV file')
 
 if __name__ == "__main__":
     input_csv_file = input("Enter input CSV file name: ")
-    output_csv_file = input("Enter output CSV file name: ")
-
-    extract_data_from_date(input_csv_file, output_csv_file)
+    extract_data_from_date(input_csv_file, "fake_clean.csv")
